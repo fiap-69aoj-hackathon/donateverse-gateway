133b4652294af64599988c02261ec81478d37420
Flavio Oliveira <flaoliveira@uolinc.com>
2020-03-26 09:14:28 -0300

    atualização config cors

--- src/main/java/com/donateverse/gateway/config/WebSecurityConfig.java
+++ src/main/java/com/donateverse/gateway/config/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
